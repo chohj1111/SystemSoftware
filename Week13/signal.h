605dd0ace9ba3129fc24053886240dd6cfe4c8c9
chohj1111 <chohyunjun1111@gmail.com>
2020-11-27 10:32:51 +0900

    201127

--- Week13/signal.h
+++
@@ -1,10 +0,0 @@
-void signal_handler(int sig) {   /*write codes to be executed when the interrupt is caught */
-}
-
-/*Calls the signal handler with the corresponding function name when the interrupt happens*/
-signal(SIGINT, signal_handler);
-
-/* Unix-style error */
-void unix_error(char *msg) {
-    fprintf(stderr, “%s: %s\n”, msg, strerror(errno));
-    exit(0);}
